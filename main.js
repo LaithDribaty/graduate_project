aa7d1dd147df4ded7be2ea4aebf0ebec9b501cbe
Laith Drebati <laith.drebaty@gmail.com>
2022-01-07 00:15:00 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+import * as THREE from './three/build/three.module.js'
+
+var clock = new THREE.Clock();
+
+var scene = new THREE.Scene();
+
+var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+
